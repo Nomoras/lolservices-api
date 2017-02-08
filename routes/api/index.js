01d55698fb06e86fb7b74c33f338b0a76885835b
Samuel Zhou <samuelzhou607@hotmail.com>
2017-02-07 22:03:06 -0500

    Fixed authentication routing

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,6 @@
 var routes = require('express').Router();
 var summoner = require("./summoner");
 
-// endpoint constants
-SUMMONER_ENDPOINT = "/summoner/:name"
-
-// api routes
-routes.post(SUMMONER_ENDPOINT, summoner.add);
-routes.delete(SUMMONER_ENDPOINT, summoner.delete);
-
 // api authentication - placeholder
 routes.use('/', function (req, res, next) {
   if (req.headers['auth'] === 'spookyghost') {
@@ -16,5 +9,9 @@ routes.use('/', function (req, res, next) {
     res.status(403).send("Unauthorized");
   }
 });
+
+// api routes
+routes.post("/summoner", summoner.add);
+routes.delete("/summoner/:name", summoner.delete);
 
 module.exports = routes;
